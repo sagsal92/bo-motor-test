d1863c6b39b821817f098886f938ea63590f7c3c
Sagar salunkhe <sagsal92@gmail.com>
2018-03-06 12:59:09 +0530

    Add files via upload

---
+++ motor_test.ino
@@ -0,0 +1,17 @@
+void setup() 
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW); 
+  delay(1000);            
+  digitalWrite(3, HIGH);                               
+  digitalWrite(2, LOW); 
+  delay(1000);                    
+}
